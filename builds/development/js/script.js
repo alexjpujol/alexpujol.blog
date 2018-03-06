7db802fa0da5f8458981bc6b4cf61b6fd4e8e545
alexjpujol <apujol@spotify.com>
2018-03-06 16:05:04 -0500

    final gulp file complete

---
+++ builds/development/js/script.js
@@ -0,0 +1,9 @@
+const Header = (function() {
+    console.log("this is the header?");
+    conosle.log("this is the VERY COOL header")
+})()
+const Test = (function() {
+    console.log("i like turtles");
+    console.log("yet another thing!");
+    console.log("how about another one!")
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     connect = require('gulp-connect'),
-    gulpif = require('gulp-if')
+    gulpif = require('gulp-if'),
+    minifyHTML = require('gulp-minify-html'),
+    minifyJSON = require('gulp-jsonminify'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
     uglify = require('gulp-uglify-es').default;
 
 let env,
@@ -24,16 +28,19 @@ if (env === 'development') {
 
 javascripts = ['components/scripts/*.js'];
 scss = ['components/sass/style.scss'];
+htmlSources = ['builds/development/*.html']
 
 gulp.task('log', function() {
     log("I like turtles!");
 });
 
+//minify html
 gulp.task("js", function () {
     return gulp.src(javascripts)
-        .pipe(concat("bundle.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(`${outputDir}js`));
+        .pipe(concat("script.js"))
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(`${outputDir}js`))
+        .pipe(connect.reload());
 });
 
 //convert Sass to CSS in a single file
@@ -43,20 +50,51 @@ gulp.task('sass', function() {
             outputStyle: sassStyle
         }))
         .pipe(gulp.dest(`${outputDir}css`))
+        .pipe(connect.reload());
+});
+
+//minify production html
+gulp.task('html', function() {
+    return gulp.src(htmlSources)
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(`${outputDir}`)))
+        .pipe(connect.reload());
+});
+
+gulp.task('images', function() {
+    return gulp.src('builds/development/images/**/*.*')
+        .pipe(gulpif(env === 'production', imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false}],
+            use: [pngcrush()]
+        })))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+        .pipe(connect.reload());
+})
+
+gulp.task('json', function() {
+    return gulp.src('builds/development/js/*.json')
+        .pipe(gulpif(env === 'production', minifyJSON()))
+        .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
+        .pipe(connect.reload());
 });
 
 //watch for changed files
 gulp.task('watch', function() {
     gulp.watch(javascripts, ['js']);
     gulp.watch('components/sass/*.scss', ['sass']);
+    gulp.watch('builds/development/*.html', ['html']);
+    gulp.watch('builds/development/js/*.json', ['json']);
+    gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 
-// gulp.task('connect', function() {
-//     connect.server({
-//         root: 'builds/development',
-//         livereload: true
-//     })
-// })
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development',
+        livereload: true
+    });
+})
 
 //run the jewels
-gulp.task('default', ['sass', 'js', 'watch']);
+gulp.task('default', ['sass', 'js', 'html', 'json', 'images', 'connect', 'watch']);
