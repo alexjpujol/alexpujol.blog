997f674755e7ae29af56f1a3cf5efff9e12d9c55
alexjpujol <apujol@spotify.com>
2018-06-15 14:12:05 -0400

    fixed height issue

--- components/scripts/Logo.js
+++ components/scripts/Logo.js
@@ -1,8 +1,8 @@
 const Logos = (()=> {
     const spotify = document.querySelector('.logo');
-    const soccer = document.querySelector('.soccer');
+    //const soccer = document.querySelector('.soccer');
     setTimeout(() => {
         spotify.style.transform = "translate(0px, 0px)"
-        soccer.style.transform = "translate(0px, 0px)";
+        //soccer.style.transform = "translate(0px, 0px)";
     }, 1000)
 })()
